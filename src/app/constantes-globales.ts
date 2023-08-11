a93b92f3e0db8c436c7e44d3d7c923b5fc87274f
Mario Palacios <desarrollador89_usi@minedu.gob.pe>
2023-08-11 14:49:01 -0500

    actualizacion ruta

--- src/app/constantes-globales.ts
+++ src/app/constantes-globales.ts
@@ -1,5 +1,5 @@
 export class ConstantesGlobales {
 
-  public static apiURL: string = "http://201.230.157.33:8081/";
+  public static apiURL: string = "https://fd6b-201-230-157-33.ngrok-free.app/";
 
 }
